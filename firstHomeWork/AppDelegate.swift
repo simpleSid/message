41c8bbf69133f7ae1861a561142d84c9d16c8326
Денис Сидоренко <itklass@mail.ru>
2019-02-17 11:27:09 +0300

    second home work start

--- firstHomeWork/AppDelegate.swift
+++ firstHomeWork/AppDelegate.swift
@@ -12,34 +12,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let logManager = LogManager()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        logManager.printCurrentState(text: "Application moved from not running state to inactive state, method:\(#function)", isLogged: logManager.isLoggedState)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        logManager.printCurrentState(text: "Application moved from \(logManager.previoslyState) state to inactive state, method:\(#function)", isLogged: logManager.isLoggedState)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        logManager.printCurrentState(text: "Application moved from inactive state to background, method:\(#function)", isLogged: logManager.isLoggedState)
-        logManager.previoslyState = "background"
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        logManager.printCurrentState(text: "Application moved from background to inactive, method:\(#function)", isLogged: logManager.isLoggedState)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        logManager.printCurrentState(text: "Application moved from inactive state to active state, method:\(#function)", isLogged: logManager.isLoggedState)
-        logManager.previoslyState = "active"
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        logManager.printCurrentState(text: "Application moved from background to not running state, method:\(#function)", isLogged: logManager.isLoggedState)
     }
 }
 
--- firstHomeWork/ViewController.swift
+++ firstHomeWork/ViewController.swift
@@ -10,43 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let logManager = LogManager()
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        logManager.isLoggedState = false
-        
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
 
     override func viewWillLayoutSubviews() {
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
     
     override func viewDidLayoutSubviews() {
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        logManager.printCurrentState(text: "\(#function)", isLogged: logManager.isLoggedState)
     }
 }
 
